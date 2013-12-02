7586f687a117238a7abcccb5f6d1078073aff6b7
aovelhanegra <rafael.reia@gmail.com>
2013-12-02 22:28:54 +0000

    updated

--- src/ProjSDistributed.c
+++ src/ProjSDistributed.c
@@ -5,16 +5,15 @@
 #include "Mover.h"
 #include <string.h>
 #define DIM 2
-int  wolfBP = 0, sqrlBP = 0, wolfStarvP = 0, genNum = 0;
+int wolfBP = 0, sqrlBP = 0, wolfStarvP = 0, genNum = 0;
 
-
-void mapping(){
-/* */
+void mapping() {
+	/* */
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
-	int teste, worldsize=0;
+	int teste, worldsize = 0;
 	FILE * inputFile;
 	int teste, i;
 	int ret = 3, x, y;
@@ -23,9 +22,9 @@ int main (int argc, char *argv[]) {
 	MPI_Status status;
 	MPI_Comm cart_comm;
 	int id, p, rank; /*id geral, p numb PRocessors, rank checkerboard*/
-	MPI_Init (&argc, &argv);
-	MPI_Comm_rank (MPI_COMM_WORLD, &id);/*id dos Processos*/
-	MPI_Comm_size (MPI_COMM_WORLD, &p); /*numero de processos*/
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);/*id dos Processos*/
+	MPI_Comm_size(MPI_COMM_WORLD, &p); /*numero de processos*/
 	int size[DIM], periods[DIM], coords[DIM], auxMap[DIM], divideX, divideY;
 
 	/*algoritmo
@@ -41,7 +40,7 @@ int main (int argc, char *argv[]) {
 	 * */
 
 	/*Distribution dynamically*/
-	size[0]=size[1]=0;
+	size[0] = size[1] = 0;
 	/* CheckBoard decomposition creation
 	 * Use MPI_DIMS_CREATE(int nodes, int ndims,int *dims);  http://www.mpi-forum.org/docs/mpi-1.1/mpi-11-html/node134.html
 	 *  MPI_CART_CREATE(MPI_Comm old_comm In - old communicator ,int dims In - grid dimensions,int *size In - # procs in each dim,
@@ -51,21 +50,21 @@ int main (int argc, char *argv[]) {
 	 * */
 
 	/*p processors, 2 dimensions (2D), size=distribution of Processors 0 for dinamicaly*/
-	MPI_Dims_create(p,2, size);
-
+	MPI_Dims_create(p, 2, size);
 
 	/*Cria canal de comunicação*/
 	/*Nota [Periodos] as Col e as Row podem ser periodicas, ou seja aceder ao -1 significa o mesmo
 	 * 	que aceder a 1 (no array) AKA wrapped, uma especie do que se pode fazer com os ecrans dos telemoveis.*/
-	periods[0]=periods[1]=0; /*Não ha periodos*/
+	periods[0] = periods[1] = 0; /*Não ha periodos*/
 
 	/*o 1 é dar premissoes para que reordene os processos para ser mais eficiente*/
-	MPI_Cart_create (MPI_COMM_WORLD, 2, size, periods, 1, &cart_comm);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, size, periods, 1, &cart_comm);
 
-
-	MPI_Comm_rank (cart_comm, &rank);/*get id after dividing*/
-	MPI_Cart_coords(cart_comm, rank/*we should not use the ID here*/, 2, coords); /* Descobre as coordenadas do Processo*/
-	printf("Process ID: %d   Process coordinates %d, %d   Process Rank %d   \n", id, coords[0], coords[1], rank);
+	MPI_Comm_rank(cart_comm, &rank);/*get id after dividing*/
+	MPI_Cart_coords(cart_comm, rank/*we should not use the ID here*/, 2,
+			coords); /* Descobre as coordenadas do Processo*/
+	printf("Process ID: %d   Process coordinates %d, %d   Process Rank %d   \n",
+			id, coords[0], coords[1], rank);
 	fflush(stdout);
 	/*TODO Vale a pena fazer Split?*/
 
@@ -84,75 +83,73 @@ int main (int argc, char *argv[]) {
 	 * */
 
 	/*			TIME		*/
-    MPI_Barrier (MPI_COMM_WORLD);
-    elapsed_time = - MPI_Wtime();
-    /*			TIME		*/
-
+	MPI_Barrier(MPI_COMM_WORLD);
+	elapsed_time = -MPI_Wtime();
+	/*			TIME		*/
 
 	/*Read Arguments*/
-    if(argc != 6){
-	if (!id) printf ("Command line: %s <inputFile, wolfBP, sqrlBP, wolfStarvP,IterationsNum >\n", argv[0]);
-	MPI_Finalize();worldsize
-	exit (1);
-    }
-    wolfBP = atoi(argv[2]);
-    sqrlBP = atoi(argv[3]);
-    wolfStarvP = atoi(argv[4]);
-    genNum = atoi(argv[5]);
+	if (argc != 6) {
+		if (!id)
+			printf(
+					"Command line: %s <inputFile, wolfBP, sqrlBP, wolfStarvP,IterationsNum >\n",
+					argv[0]);
+		MPI_Finalize();
+		worldsize
+		exit(1);
+	}
+	wolfBP = atoi(argv[2]);
+	sqrlBP = atoi(argv[3]);
+	wolfStarvP = atoi(argv[4]);
+	genNum = atoi(argv[5]);
 
-
-
-    /*Master Read The file*/
-    if(id == 0){
+	/*Master Read The file*/
+	if (id == 0) {
 		inputFile = fopen(argv[1], "r");
 		teste = fscanf(inputFile, "%d", &worldsize);
-		if(teste != 1){
+		if (teste != 1) {
 			printf("Input error!\n");
 			exit(-1);
 		}
-    }
+	}
 
 	/*Mapping*/
-    if(id == 0){
-    	/*Arranja coordenadas da disposiçao do Mundo*/
-    	MPI_Cart_coords(cart_comm, p, DIM,auxMap);
-    	divideX = auxMap[0] + 1;/*TODO check This!*/
-    	divideY = auxMap[1] + 1;
-    	/*Check floor and ceiling*/
-    	/*0 vai de 0 até N/x e 0 até N/Y*/
-    	/*1 vao de N/x até 2N/X e N/Y até 2N/Y*/
-    	/*P vai de PN/X N e de PN/Y ate N*/
+	if (id == 0) {
+		/*Arranja coordenadas da disposiçao do Mundo*/
+		MPI_Cart_coords(cart_comm, p, DIM, auxMap);
+		divideX = auxMap[0] + 1; /*TODO check This!*/
+		divideY = auxMap[1] + 1;
+		/*Check floor and ceiling*/
+		/*0 vai de 0 até N/x e 0 até N/Y*/
+		/*1 vao de N/x até 2N/X e N/Y até 2N/Y*/
+		/*P vai de PN/X N e de PN/Y ate N*/
 
-    }
-	/*Read & distribute the Matrix*/
-    if(id == 0){
-    	while(ret != 3){
-    		for(;;){
-    			/*guarda num buffer*/
-    			if(/*Testa limites da coisa*/){
-    				rank++;
-    				/*Envia o buffer*/
-    			}
-    		}
-    	}
-    }
-    else{
-    	/*Le com a tag certa e afins*/
+		/*Read & distribute the Matrix*/
+		while (ret != 3) {
+			for (;;) { /*TODO*/
+				/*guarda num buffer*/
+				if (/*Testa limites da coisa*/) {
+					rank++;
+					/*Envia o buffer*/
+				}
+			}
+		}
+	} else {
+		/*Le com a tag certa e afins*/
 
-    }
-    /*So o 0 Le
-     * LE para o 0;
-     * Le e guarda num buffer para o 1;
-     * Ve quando passa a divisão;
-     * envia par ao 1;
-     * faz rank++
-     * Repeat
-     * */
-    /*		GAME TIME		*/
-    game_time = - MPI_Wtime();
-    /*Run game*/
-    game_time += MPI_Wtime();
-    /*		GAME TIME		*/
+	}
+	/*So o 0 Le
+	 * LE para o 0;
+	 * Le e guarda num buffer para o 1;
+	 * Ve quando passa a divisão;
+	 * envia par ao 1;
+	 * faz rank++
+	 * Repeat
+	 * */
+	/*		GAME TIME		*/
+	game_time = -MPI_Wtime();
+	/*Run game*/
+	game_time += MPI_Wtime();
+	/*		GAME TIME		*/
 
 	/*Ending the Program*/
 	elapsed_time += MPI_Wtime(); /*Calcula o tempo*/
